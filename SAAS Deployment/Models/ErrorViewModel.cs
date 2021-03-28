8cc309b18715b7bfb3b14236c30eef02aeb468f6
Andrew Wong <kkwong125@gmail.com>
2021-03-28 15:01:39 -0700

    generated login

--- SAAS Deployment/Models/ErrorViewModel.cs
+++ SAAS Deployment/Models/ErrorViewModel.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace SAAS_Deployment.Models
-{
-    public class ErrorViewModel
-    {
+namespace SAAS_Deployment.Models {
+    public class ErrorViewModel {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
