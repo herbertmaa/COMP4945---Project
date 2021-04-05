283d9e148948bfdb0a9faa2b7d024d8f97e0e9f5
chiawei chang <eric.c.chang@outlook.com>
2021-04-04 23:10:53 -0700

    Client Test v2

--- Unit Tests/ClientTest.cs
+++ Unit Tests/ClientTest.cs
@@ -1,32 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using OpenQA.Selenium;
+using System;
+using System.IO;
 using OpenQA.Selenium.Chrome;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Threading;
 
 
-namespace ClientTests.Test
+namespace Unit_Tests
 {
+    [TestClass]
     class ClientTest
     {
-        [TestMethod]
-        public void CreateClient()
+        private ChromeDriver CreateDriver()
         {
-            string url = "https://localhost:44372/Client";
-            ChromeDriver driver = new ChromeDriver();
+            string path = Directory.GetCurrentDirectory();
+            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\chromeDriver\"));
+            return new ChromeDriver(newPath);
+        }
+        [Test, Order(1)]
+        public void CreateEmployee()
+        {
+            string url = "https://localhost:44372/Clients";
+            ChromeDriver driver = CreateDriver();
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(By.Id("Input_Email")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Email")).SendKeys("admin@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).SendKeys("password");
+            driver.FindElement(By.ClassName("btn-primary")).Click();
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.FindElement(By.LinkText("Create New")).Click();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.FindElement(By.Id("FirstName")).SendKeys("MRRRR");
             driver.FindElement(By.Id("LastName")).SendKeys("Handsome");
-            driver.FindElement(By.Id("Email Address")).SendKeys("handsome@gmail.com");
+            driver.FindElement(By.Id("Email")).SendKeys("handsome@gmail.com");
             driver.FindElement(By.Id("Branch")).SendKeys("LOHA");
             driver.FindElement(By.Id("Address")).SendKeys("123 road");
             driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            driver.Quit();
 
+        }
+
+        [Test, Order(2)]
+        public void EditClient()
+        {
+            string url = "https://localhost:44372/Clients";
+            ChromeDriver driver = CreateDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(By.Id("Input_Email")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Email")).SendKeys("admin@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).SendKeys("password");
+            driver.FindElement(By.ClassName("btn-primary")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            //EDIT Client CLICK
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("FirstName")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("FirstName")).SendKeys("MSSSS");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys("UGLY");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys("clientlTest1@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Branch")).Clear();
+            driver.FindElement(By.Id("Branch")).SendKeys("Edited Branch Test11");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Address")).Clear();
+            driver.FindElement(By.Id("Address")).SendKeys("Delta");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+
+        [Test, Order(3)]
+        public void DetailsEditClient()
+        {
+            string url = "https://localhost:44372/Clients";
+            ChromeDriver driver = CreateDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.FindElement(By.Id("Input_Email")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Email")).SendKeys("admin@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).SendKeys("password");
+            driver.FindElement(By.ClassName("btn-primary")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            //DETAIL CLIENT CLICK
+            driver.FindElement(By.LinkText("Details")).Click();
+            Thread.Sleep(2000);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.LinkText("Edit")).Click();
+            Thread.Sleep(2000);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.LinkText("Back to List")).Click();
+        }
+
+
+        [Test, Order(4)]
+        public void DeleteClient()
+        {
+            string url = "https://localhost:44372/Clients";
+            ChromeDriver driver = CreateDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+
+            driver.FindElement(By.Id("Input_Email")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Email")).SendKeys("admin@gmail.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.Id("Input_Password")).SendKeys("password");
+            driver.FindElement(By.ClassName("btn-primary")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            //DELETE CLIENT CLICK
+            driver.FindElement(By.LinkText("Delete")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
         }
 
     }
